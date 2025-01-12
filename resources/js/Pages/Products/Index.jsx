3a1b77dd9b6bef0581b9c1977b9f465e636de276
AnthonyAragao <anthony.amoz.s.a@gmail.com>
2025-01-12 01:44:44 -0300

    :sparkles: feat: Adicionado rotas para o controller de produtos

--- resources/js/Pages/Products/Index.jsx
+++ resources/js/Pages/Products/Index.jsx
@@ -1,19 +1,11 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Index({ products }) {
     console.log(products);
-
+    
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Products
-                </h2>
-            }
-        >
+        <div>
             <Head title="Products" />
-
-        </AuthenticatedLayout>
+        </div>
     );
 }
